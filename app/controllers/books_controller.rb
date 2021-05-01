efb13c8e36d750503edbe33f3eb1357a17831d0a
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-01 00:22:38 +0000

    [Add]books/新規投稿・保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
+  def create
+    book=Book.new
+    book.save
+    redirect_to books_path
+  end
+
   def index
+    @book=Book.new
   end
 
   def show
