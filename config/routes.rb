03de6cdb85c3481761d6d2309e8305322b448195
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-14 06:37:29 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  namespace :public do
+    resources :items
+    resources :cart_items do
+      collection do
+        delete 'destroy_all'
+      end
+  end
+  end
 end
